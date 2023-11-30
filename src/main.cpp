31dbcf186a23749622d1b1e1ceb76782f4411ea8
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-12-01 00:39:31 +1100

    Add tetris cells to window

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,40 @@
 #include <SFML/Graphics.hpp>
+#include <algorithm>
+#include <iostream>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+int constexpr GAP = 7, ROW_COUNT = 12, COL_COUNT = 20, CELL_SIZE = 50;
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+void drawCells(sf::RenderWindow &window) {
+  float length = 100, gap = 10;
+  sf::RectangleShape squareCell(sf::Vector2f(10.f, 10.f));
+  squareCell.setFillColor(sf::Color(180, 50, 20));
+  squareCell.setSize(sf::Vector2f(length, length));
 
-        window.clear();
-        window.display();
+  float x = 10, y = 10;
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 7; j++) {
+      squareCell.setPosition(x, y);
+      window.draw(squareCell);
+      x += length + gap;
     }
+    y += length + gap;
+    x = gap;
+  }
+}
+
+int main() {
+  auto window = sf::RenderWindow({670u, 1000u}, "CMake SFML Project");
+  window.setFramerateLimit(144);
+
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    drawCells(window);
+    window.display();
+  }
 }
