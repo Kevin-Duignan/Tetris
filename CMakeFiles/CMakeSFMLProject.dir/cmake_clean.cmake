f4ffecd0b96b45a2e85bc189e130ec3a7541d25c
AhnafMasud10 <ahnafmasud11@gmail.com>
2023-12-01 07:57:16 +0600

    cmake working

---
+++ CMakeFiles/CMakeSFMLProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeSFMLProject.dir/src/main.cpp.o"
+  "CMakeFiles/CMakeSFMLProject.dir/src/main.cpp.o.d"
+  "bin/CMakeSFMLProject"
+  "bin/CMakeSFMLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeSFMLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/CMakeSFMLProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CMakeSFMLProject.
+# This may be replaced when dependencies are built.
