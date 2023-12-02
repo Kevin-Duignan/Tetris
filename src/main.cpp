c57891154ce65a8e4742ecdbd790aa9da3ad2db9
harperj34 <61570199+harperj34@users.noreply.github.com>
2023-12-02 15:48:50 +1100

    Moves on tick
    Can move down, ledft, right. Moves on every tick.
    Doesn't stop at the right location because of logic error with columns and rows.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,20 @@ int constexpr GAP = 4, ROWS = 12, COLUMNS = 20, CELL_SIZE = 25;
 
 // Types
 typedef std::array<std::array<int, COLUMNS>, ROWS> matrixType;
+typedef std::tuple<int, int> coords; // (x, y)
+typedef std::vector<coords>
+    pieceType; // NOT PERMANENT. TO be replaced with piece struct.
+
+// Enums
+enum blockType {
+  empty,
+  active,
+  sealed
+}; // empty means there is no block, acive means there is a block that can move,
+   // sealed means there is a block that can't move.
+
+// Prototypes
+pieceType movePiece(matrixType &matrix, pieceType piece, char direction);
 
 void drawCells(sf::RenderWindow &window, matrixType matrix) {
   // Set size dynamically to ensure all cells are drawn
@@ -18,16 +32,18 @@ void drawCells(sf::RenderWindow &window, matrixType matrix) {
 
   sf::RectangleShape block(sf::Vector2f(CELL_SIZE, CELL_SIZE));
   block.setFillColor(sf::Color(255, 0, 0));
+
   float x = GAP, y = GAP;
   for (int i = 0; i < COLUMNS; i++) {
     for (int j = 0; j < ROWS; j++) {
-      if (matrix[j][i] == 0) {
+      if (matrix[j][i] == empty) {
         cell.setPosition(x, y);
         window.draw(cell);
       } else { // there is a block in that spot
         block.setPosition(x, y);
         window.draw(block);
       }
+      x += CELL_SIZE + GAP;
     }
     y += CELL_SIZE + GAP;
     x = GAP;
@@ -39,9 +55,8 @@ int main() {
   for (auto &row : matrix) {
     std::fill(row.begin(), row.end(), 0);
   }
-  matrix[1][2] = 1;
 
-  for (const auto &row : matrix) {
+  for (const auto &row : matrix) { // Debugging only!
     for (const auto &cell : row) {
       std::cout << cell << ' ';
     }
@@ -54,6 +69,13 @@ int main() {
                        "CMake SFML Project");
   window.setFramerateLimit(144);
 
+  sf::Clock clock;                      // starts the clock
+  sf::Time gameTick = sf::seconds(0.2); // game tick every 1 second
+
+  pieceType startPiece = {
+      std::make_tuple(0, 0),
+      std::make_tuple(1, 0)}; // Temp implementation, don't keep!
+
   while (window.isOpen()) {
     for (auto event = sf::Event{}; window.pollEvent(event);) {
       if (event.type == sf::Event::Closed) {
@@ -64,5 +86,75 @@ int main() {
     window.clear();
     drawCells(window, matrix);
     window.display();
+
+    // remove the old piece that is active
+    for (coords c : startPiece) {
+      matrix[std::get<0>(c)][std::get<1>(c)] = empty;
+    }
+
+    if (clock.getElapsedTime() > gameTick) { // game tick
+      clock.restart();                       // Reset the clock
+
+      startPiece = movePiece(matrix, startPiece, 'd');
+    }
+
+    // replace the moved (or not) active piece.
+    for (coords c : startPiece) {
+      matrix[std::get<0>(c)][std::get<1>(c)] = active;
+    }
+
+    // if move button clicked, try that move
   }
+}
+pieceType movePiece(matrixType &matrix, pieceType piece, char direction) {
+
+  // Function to check if the new position is valid
+  auto isValidPosition = [&](int x, int y) { // lambda!
+    return (x >= 0 && x < COLUMNS && y >= 0 && y < ROWS &&
+            matrix[y][x] != sealed);
+  };
+
+  // Move piece left
+  if (direction == 'l') {
+    for (coords &c : piece) {
+      int newX = std::get<0>(c) - 1;
+      int newY = std::get<1>(c);
+      if (!isValidPosition(newX, newY)) {
+        return piece; // Can't move left
+      }
+    }
+    for (coords &c : piece) {
+      std::get<0>(c)--; // Move each block left
+    }
+  }
+
+  // Move piece down
+  else if (direction == 'd') {
+    for (coords &c : piece) {
+      int newX = std::get<0>(c);
+      int newY = std::get<1>(c) + 1;
+      if (!isValidPosition(newX, newY)) {
+        return piece; // Can't move down
+      }
+    }
+    for (coords &c : piece) {
+      std::get<1>(c)++; // Move each block down
+    }
+  }
+
+  // Move piece right
+  else if (direction == 'r') {
+    for (coords &c : piece) {
+      int newX = std::get<0>(c) + 1;
+      int newY = std::get<1>(c);
+      if (!isValidPosition(newX, newY)) {
+        return piece; // Can't move right
+      }
+    }
+    for (coords &c : piece) {
+      std::get<0>(c)++; // Move each block right
+    }
+  }
+
+  return piece; // Return the updated piece
 }
