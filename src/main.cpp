c41b6a295643e984cad3f48565073682cb374458
AhnafMasud10 <ahnafmasud11@gmail.com>
2023-12-02 16:44:23 +0600

    incomplete struct code

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,157 @@
 #include <SFML/Graphics.hpp>
-#include <vector>
+#include <algorithm>
+#include <array>
+#include <iostream>
 
-struct Tetromino {
-    sf::Color color;
-    std::vector<sf::Vector2i> blocks;
+int constexpr GAP = 4, ROWS = 20, COLUMNS = 12, CELL_SIZE = 25;
 
-    Tetromino(sf::Color c, std::vector<sf::Vector2i> b) : color(c), blocks(b) {}
+// Types
+typedef std::array<std::array<int, COLUMNS>, ROWS> matrixType;
+typedef std::tuple<int, int> coords; // (x, y)
+// NOT PERMANENT. TO be replaced with piece struct.
+typedef std::vector<coords> pieceType;
 
-    void draw(sf::RenderWindow& window, int cellSize) const {
-        sf::RectangleShape blockShape(sf::Vector2f(cellSize, cellSize));
-        blockShape.setFillColor(color);
+// Enums
+enum cellType {
+  empty,
+  active,
+  sealed
+}; // empty means there is no block, acive means there is a block that can move,
+   // sealed means there is a block that can't move.
 
-        for (const auto& block : blocks) {
-            blockShape.setPosition(block.x * cellSize, block.y * cellSize);
-            window.draw(blockShape);
-        }
+// Prototypes
+pieceType movePiece(matrixType &matrix, pieceType piece, char direction);
+
+void drawCells(sf::RenderWindow &window, matrixType matrix) {
+
+  sf::RectangleShape cell(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+  cell.setFillColor(sf::Color(180, 50, 20));
+
+  sf::RectangleShape block(sf::Vector2f(CELL_SIZE, CELL_SIZE));
+  block.setFillColor(sf::Color(255, 0, 0));
+
+  float x = GAP, y = GAP;
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLUMNS; j++) {
+      if (matrix[j][i] == empty) {
+        cell.setPosition(x, y);
+        window.draw(cell);
+      } else { // there is a block in that spot
+        block.setPosition(x, y);
+        window.draw(block);
+      }
+      x += CELL_SIZE + GAP;
     }
-};
+    y += CELL_SIZE + GAP;
+    x = GAP;
+  }
+}
+
+int main() {
+  matrixType matrix;
+  for (auto &row : matrix) {
+    std::fill(row.begin(), row.end(), 0);
+  }
+
+  for (const auto &row : matrix) { // Debugging only!
+    for (const auto &cell : row) {
+      std::cout << cell << ' ';
+    }
+    std::cout << '\n';
+  }
+
+  auto window =
+      sf::RenderWindow(sf::VideoMode((CELL_SIZE + GAP) * COLUMNS + GAP,
+                                     (CELL_SIZE + GAP) * ROWS + GAP),
+                       "CMake SFML Project");
+  window.setFramerateLimit(144);
+
+  sf::Clock clock;                      // starts the clock
+  sf::Time gameTick = sf::seconds(0.2); // game tick every 1 second
+
+  pieceType startPiece = {
+      std::make_tuple(0, 0),
+      std::make_tuple(1, 0)}; // Temp implementation, don't keep!
+
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event);) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    drawCells(window, matrix);
+    window.display();
+
+    // remove the old piece that is active
+    for (coords c : startPiece) {
+      matrix[std::get<0>(c)][std::get<1>(c)] = empty;
+    }
+
+    if (clock.getElapsedTime() > gameTick) { // game tick
+      clock.restart();                       // Reset the clock
+
+      startPiece = movePiece(matrix, startPiece, 'd');
+    }
+
+    // replace the moved (or not) active piece.
+    for (coords c : startPiece) {
+      matrix[std::get<0>(c)][std::get<1>(c)] = active;
+    }
+
+    // if move button clicked, try that move
+  }
+}
+pieceType movePiece(matrixType &matrix, pieceType piece, char direction) {
+
+  // Function to check if the new position is valid
+  auto isValidPosition = [&](int x, int y) { // lambda!
+    return (x >= 0 && x < COLUMNS && y >= 0 && y < ROWS &&
+            matrix[y][x] != sealed);
+  };
+
+  // Move piece left
+  if (direction == 'l') {
+    for (coords &c : piece) {
+      int newX = std::get<0>(c) - 1;
+      int newY = std::get<1>(c);
+      if (!isValidPosition(newX, newY)) {
+        return piece; // Can't move left
+      }
+    }
+    for (coords &c : piece) {
+      std::get<0>(c)--; // Move each block left
+    }
+  }
+
+  // Move piece down
+  else if (direction == 'd') {
+    for (coords &c : piece) {
+      int newX = std::get<0>(c);
+      int newY = std::get<1>(c) + 1;
+      if (!isValidPosition(newX, newY)) {
+        return piece; // Can't move down
+      }
+    }
+    for (coords &c : piece) {
+      std::get<1>(c)++; // Move each block down
+    }
+  }
+
+  // Move piece right
+  else if (direction == 'r') {
+    for (coords &c : piece) {
+      int newX = std::get<0>(c) + 1;
+      int newY = std::get<1>(c);
+      if (!isValidPosition(newX, newY)) {
+        return piece; // Can't move right
+      }
+    }
+    for (coords &c : piece) {
+      std::get<0>(c)++; // Move each block right
+    }
+  }
+
+  return piece; // Return the updated piece
+}
---
+++ src/tetromino.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+struct Tetromino {
+    sf::Color color;
+    std::vector<sf::Vector2i> blocks;
+
+    Tetromino(sf::Color c, std::vector<sf::Vector2i> b) : color(c), blocks(b) {}
+
+    void draw(sf::RenderWindow& window, int cellSize) const {
+        sf::RectangleShape blockShape(sf::Vector2f(cellSize, cellSize));
+        blockShape.setFillColor(color);
+
+        for (const auto& block : blocks) {
+            blockShape.setPosition(block.x * cellSize, block.y * cellSize);
+            window.draw(blockShape);
+        }
+    }
+
+
+}
