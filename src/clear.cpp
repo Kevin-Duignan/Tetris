1f11002a9a96759dd6371500213c84b436ccb42e
harperj34 <61570199+harperj34@users.noreply.github.com>
2023-12-14 16:51:19 +1100

    Clears rows correctly

--- src/clear.cpp
+++ src/clear.cpp
@@ -3,22 +3,32 @@
 #include <iostream>
 
 bool shouldClear(std::array<int, COLUMNS> row);
+void moveRowsDown(matrixType &matrix, int movable);
 
-void clearRows(matrixType &matrix) {
-  for (const auto &row : matrix) {
+void clearRows(
+    matrixType &matrix) { // consider updating so it goes from the bottom first.
+  int i = 0;
+  for (auto &row : matrix) {
     if (shouldClear(row)) {
-      return;
+      std::fill(row.begin(), row.end(), 0);
+      moveRowsDown(matrix, i);
     }
+    i++;
   }
 }
 
 bool shouldClear(std::array<int, COLUMNS> row) {
   for (int number : row) {
-    std::cout << number;
     if (number != 2) {
       return false;
     }
   }
   std::cout << "yes";
   return true;
+}
+
+void moveRowsDown(matrixType &matrix, int movable) {
+  for (int i = movable; i > 0; i--) {
+    matrix[i] = matrix[i - 1];
+  }
 }
