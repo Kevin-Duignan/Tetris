7c29da00256fe178580eab4050536a7499d47e14
harperj34 <61570199+harperj34@users.noreply.github.com>
2023-12-15 14:04:07 +1100

    Colours working

---
+++ headers/colours.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+sf::Color pastel_yellow_light = sf::Color(245, 227, 181);
+sf::Color pastel_yellow_dark = sf::Color(245, 208, 115);
+sf::Color black = sf::Color(0, 0, 0);
