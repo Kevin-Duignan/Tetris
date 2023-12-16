b59c78c82d4a95221d4bf3d8fddfebd62d24a6f5
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-12-17 04:07:32 +1100

    Refactor movePiece to use tuple unpacking for coords and offset

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -10,40 +10,42 @@ coords movePiece(matrixType &matrix, pieceCoords piece, char direction,
             matrix[y][x] != std::to_underlying(cellType::sealed));
   };
 
+  auto [offset_x, offset_y] = offset;
+
   // Move piece left
   if (direction == 'l') {
-    for (coords &c : piece) {
-      int newX = std::get<0>(c) + std::get<0>(offset) - 1;
-      int newY = std::get<1>(c) + std::get<1>(offset);
+    for (auto &[c_x, c_y] : piece) {
+      int newX = c_x + offset_x - 1;
+      int newY = c_y + offset_y;
       if (!isValidPosition(newX, newY)) {
         return offset; // Can't move left
       }
     }
-    return std::make_tuple(std::get<0>(offset) - 1, std::get<1>(offset));
+    return std::make_tuple(offset_x - 1, offset_y);
   }
 
   // Move piece down
   else if (direction == 'd') {
-    for (coords &c : piece) {
-      int newX = std::get<0>(c) + std::get<0>(offset);
-      int newY = std::get<1>(c) + std::get<1>(offset) + 1;
+    for (auto &[c_x, c_y] : piece) {
+      int newX = c_x + offset_x;
+      int newY = c_y + offset_y + 1;
       if (!isValidPosition(newX, newY)) {
         return offset; // Can't move down
       }
     }
-    return std::make_tuple(std::get<0>(offset), std::get<1>(offset) + 1);
+    return std::make_tuple(offset_x, offset_y + 1);
   }
 
   // Move piece right
   else if (direction == 'r') {
-    for (coords &c : piece) {
-      int newX = std::get<0>(c) + std::get<0>(offset) + 1;
-      int newY = std::get<1>(c) + std::get<1>(offset);
+    for (auto &[c_x, c_y] : piece) {
+      int newX = c_x + offset_x + 1;
+      int newY = c_y + offset_y;
       if (!isValidPosition(newX, newY)) {
         return offset; // Can't move right
       }
     }
-    return std::make_tuple(std::get<0>(offset) + 1, std::get<1>(offset));
+    return std::make_tuple(offset_x + 1, offset_y);
   }
   return offset;
 }
