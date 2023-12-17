f79a1a5598734e27f1298525ae8ed088e9943461
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-12-17 18:38:58 +1100

    Change colour presets for more contrast

--- headers/colours.hpp
+++ headers/colours.hpp
@@ -2,17 +2,17 @@
 #include <SFML/Graphics.hpp>
 #include <map>
 
-const sf::Color PASTEL_YELLOW_LIGHT = sf::Color(255, 216, 165);
-const sf::Color PASTEL_YELLOW_DARK = sf::Color(183, 151, 110);
-const sf::Color BROWN = sf::Color(65, 54, 43);
+const sf::Color PASTEL_YELLOW_LIGHT = sf::Color(225, 196, 135);
+const sf::Color PASTEL_YELLOW_DARK = sf::Color(153, 121, 80);
+const sf::Color BROWN = sf::Color(75, 64, 53);
 
 using RGB = std::tuple<std::uint8_t, std::uint8_t, std::uint8_t>;
 
 // Piece colours (not initialized with sf::Color yet so can use constexpr)
-constexpr RGB GREEN = std::make_tuple(168, 228, 160);
-constexpr RGB BLUE = std::make_tuple(159, 223, 232);
-constexpr RGB PURPLE = std::make_tuple(221, 160, 221);
-constexpr RGB RED = std::make_tuple(255, 105, 97);
-constexpr RGB ORANGE = std::make_tuple(255, 179, 71);
-constexpr RGB PINK = std::make_tuple(255, 105, 180);
-constexpr RGB CYAN = std::make_tuple(87, 199, 208);
+constexpr RGB GREEN = std::make_tuple(76, 187, 23);
+constexpr RGB BLUE = std::make_tuple(70, 130, 180);   // Darker pastel blue
+constexpr RGB PURPLE = std::make_tuple(127, 92, 199); // Darker pastel purple
+constexpr RGB RED = std::make_tuple(205, 92, 92);     // Darker pastel red
+constexpr RGB ORANGE = std::make_tuple(255, 140, 0);  // Darker pastel orange
+constexpr RGB PINK = std::make_tuple(255, 105, 180);  // Darker pastel pink
+constexpr RGB CYAN = std::make_tuple(0, 139, 139);    // Darker pastel cyan
