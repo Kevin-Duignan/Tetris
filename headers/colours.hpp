8411ef01b504a1bf1c05ca80a80c266841d5d20c
Kevin Duignan <kevin.nguyen200476@gmail.com>
2023-12-17 16:03:36 +1100

    Add pastel colour presets

--- headers/colours.hpp
+++ headers/colours.hpp
@@ -4,3 +4,12 @@
 sf::Color pastel_yellow_light = sf::Color(255, 216, 165);
 sf::Color pastel_yellow_dark = sf::Color(183, 151, 110);
 sf::Color brown = sf::Color(65, 54, 43);
+
+// Piece colours
+sf::Color pastel_green = sf::Color(168, 228, 160);
+sf::Color pastel_blue = sf::Color(159, 223, 232);
+sf::Color pastel_purple = sf::Color(221, 160, 221);
+sf::Color pastel_red = sf::Color(255, 105, 97);
+sf::Color pastel_orange = sf::Color(255, 179, 71);
+sf::Color pastel_pink = sf::Color(255, 105, 180);
+sf::Color pastel_cyan = sf::Color(87, 199, 208);
