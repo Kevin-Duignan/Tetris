ef7685544ece24d798f1200b1ffed49934834e60
harperj34 <61570199+harperj34@users.noreply.github.com>
2023-12-17 11:27:19 +1100

    remove brown

--- headers/colours.hpp
+++ headers/colours.hpp
@@ -4,4 +4,3 @@
 sf::Color pastel_yellow_light = sf::Color(255, 216, 165);
 sf::Color pastel_yellow_dark = sf::Color(183, 151, 110);
 sf::Color brown = sf::Color(65, 54, 43);
-sf::Color black = sf::Color(0, 0, 0);
